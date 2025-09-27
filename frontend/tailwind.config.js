762c77c17b2304b839a2bdb155e0700278d28982
Uttam <uttamrohit4545@gmail.com>
2025-09-27 11:17:59 +0600

    feat : extend theme color setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#181A2A',
+        'secondary': '#4B6BFB',
+        'accent' : '#E8E8EA'
+      },
+    },
+  },
+  plugins: [],
+}
