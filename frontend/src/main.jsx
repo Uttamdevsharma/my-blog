64a489bc918349ba9750b6ae5fffc8045accf404
Uttam <uttamrohit4545@gmail.com>
2025-09-27 12:01:36 +0600

    feat : nested routes using react routers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element : <div>Home</div>
+      },
+      {
+        path: '/blogs',
+        element : <div>Blogs</div>
+      },
+      {
+        path: '/about',
+        element : <div>About page</div>
+      },
+      {
+        path: '/contact',
+        element : <div>contact page</div>
+      },
+
+    ]
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />,
   </StrictMode>,
 )
