dd700ed176817ea81d55a1b3830761aa1fda7a6c
Uttam <uttamrohit4545@gmail.com>
2025-09-27 12:10:26 +0600

    feat : navbar implement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
  
   return (
     <>
+    <Navbar/>
     <Outlet/>
+    <footer>Footer</footer>
     </> 
   )
 }
