d47ea7d34a491bcafb97eddeaa92941b8a4ac2cd
Uttam <uttamrohit4545@gmail.com>
2025-09-27 22:20:15 +0600

    feat: navbar logo and menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
@@ -6,7 +8,47 @@ const Navbar = () => {
     <nav className='bg-white shadow-md'>
 
      {/* desktop menu and mobile button */}
-      <div className='container max-w-7xl mx-auto px-4 py-3 '>Desktop</div>
+      <div className='container max-w-7xl mx-auto px-4 py-3  flex justify-between '>
+        {/* logo */}
+        <div>
+          <a href='/'>
+          <img src="/logo.png" alt="logo" />
+          </a>
+        </div>
+
+
+        {/* menu items */}
+        <ul className='md:flex hidden gap-4'>
+          <li>
+            <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-500" : "hover:text-gray-800"}>
+          Home
+        </NavLink>  
+          </li>
+
+          <li>
+            <NavLink to='/blogs' className={({ isActive }) => isActive ? "text-blue-500" : "hover:text-gray-800"}>
+          Blogs
+        </NavLink>  
+          </li>
+
+          <li>
+            <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-500" : "hover:text-gray-800"}>
+          About
+        </NavLink>  
+          </li>
+
+          <li>
+            <NavLink to='/contact' className={({ isActive }) => isActive ? "text-blue-500" : "hover:text-gray-800"}>
+          Contact
+        </NavLink>  
+          </li>
+
+        </ul>
+
+        {/* search and toggle */}
+        <div>Search</div>
+
+      </div>
 
       {/* mobile menu items */}
       <div className='hidden'>Mobile menu items</div>
