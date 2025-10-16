518081d9b07c63630cdae0e512531a6757647297
Uttam <uttamrohit4545@gmail.com>
2025-10-16 16:55:07 +0600

    feat : delete a post by id

--- server/src/routes/blog.routes.js
+++ server/src/routes/blog.routes.js
@@ -3,10 +3,54 @@ const Blog = require('../models/blog.model')
 
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.send("From Blog routes")
+
+//get all blogs
+router.get('/', async(req,res) => {
+    try{
+        const blogs = await Blog.find().sort({createdAt : -1})
+
+        res.send({
+            message : "succesfully fetching all blogs",
+            blogs
+        })
+
+    }catch(error){
+        res.status(500).send({
+            message : "Error create a  new blog",
+            error
+    })
+}
+    
 })
 
+
+
+//get a single blog by id
+router.get('/:id' , async(req,res) => {
+    const {id} = req.params
+    try{
+        const blog = await Blog.findById(id);
+        if(!blog) {
+            res.send({message : "not blog found"})
+        } else {
+            res.send({
+                message : "successfully fetched",
+                blog
+            })
+        }
+
+    }catch(error){
+        res.status(500).send({
+            message : "Error create a  new blog",
+            error
+        })
+    }
+
+})
+
+
+
+//post a new blog
 router.post('/add-post', async(req,res) => {
 
     try{
@@ -25,6 +69,28 @@ router.post('/add-post', async(req,res) => {
         })
     }
    
+})
+
+//delete a blog by id
+router.delete("/:id" , async(req,res) => {
+    const {id} = req.params
+    try{
+        const deleteBlog = await Blog.findByIdAndDelete(id)
+    if(!deleteBlog) {
+        res.status(404).send({message : "not FOUND"})
+    } 
+    res.status(200).send({
+        message : "Deleted Succesfully",
+        deleteBlog
+    })
+ }catch(error) {
+    res.status(500).send({
+        message : "Error create a  new blog",
+        error
+    })
+
+ }
+    
 })
 
 
