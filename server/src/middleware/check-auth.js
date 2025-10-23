b035d8fdfcdc2410146f874d2fa8dd60885b5533
Uttam <uttamrohit4545@gmail.com>
2025-10-23 15:09:43 +0600

    feat:check authentication

--- server/src/middleware/check-auth.js
+++ server/src/middleware/check-auth.js
@@ -15,7 +15,6 @@ function checkAuthentication(req,res,next) {
 
     try{
         const decoded = jwt.verify(token , process.env.JWT_SECRET)
-        console.log(decoded)
         req.user = decoded;
         next();
     } catch (error) {
